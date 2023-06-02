c41cdb19a87860f5b7b2c89d093e4ef0140abdb8
kalitvyan <kalitvyan@ilink.dev>
2023-06-02 15:04:31 +0700

    Fix error with XDebug log file (#25)

--- docker/php/dev/xdebug.sh
+++ docker/php/dev/xdebug.sh
@@ -4,6 +4,7 @@ if [ "$ENV" == "dev" ] || [ "$ENV" == "test" ]; then
     pecl install xdebug
     docker-php-ext-enable xdebug
     mv /tmp/xdebug.ini /usr/local/etc/php/conf.d/
+    touch /var/log/xdebug.log
 else
     rm /tmp/xdebug.ini
 fi
